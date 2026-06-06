220a83e80ada2d8ec7020693dce098b2e175a347
Yasin Miyade <yasinmiyade66@gmail.com>
2026-06-07 00:08:21 +0530

    Update index.js

--- backend/server/index.js
+++ backend/server/index.js
@@ -18,7 +18,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Enable CORS and JSON parsing
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://its-myportfolio.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Prevent browser caching on all API requests
